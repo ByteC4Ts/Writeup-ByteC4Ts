ec30a181058bfff81d199a359015b6f409a1d4a7
0xf2 <nqaisme@icloud.com>
2024-11-21 14:48:04 +0700

    luc1f3r krypto

---
+++ 1337-Up-Live/krypto/schodinger/README.md
@@ -0,0 +1,2 @@
+#### [1337 Live Up CTF](https://ctf.intigriti.io/)
+##### [Schrödinger's Pad - krypto challenge](https://ctf.intigriti.io/challenges#Schr%C3%B6dinger's%20Pad-29)
---
+++ 1337-Up-Live/krypto/schodinger/solve.py
@@ -0,0 +1,39 @@
+from pwn import *
+
+PAYLOAD = b"luciferrrr" * 16
+
+
+def check():
+    io = remote("pad.ctf.intigriti.io", 1348)
+    io.recvuntil(b"Encrypted (cat state=ERROR! 'cat not in box'): ")
+    flag_encrypted = bytes.fromhex(io.recvline().decode().strip())
+    io.recvuntil(b"\nAnyway, why don't you try it for yourself?\n")
+    io.sendline(PAYLOAD)
+    s = io.recvline().decode().strip().split(": ")
+    state = "alive" in s[0]
+    c_cipher = bytes.fromhex(s[1])
+    io.close()
+    return state, flag_encrypted, c_cipher
+
+
+def ret(c):
+    res = bytearray()
+    for byte in c:
+        res.append((byte ^ 0xAC) >> 1)
+    return res
+
+
+def main():
+    state, flag_encrypted, c_cipher = check()
+    while not state:
+        state, flag_encrypted, c_cipher = check()
+    key = xor(ret(c_cipher), PAYLOAD)
+    flag = xor(flag_encrypted, key)
+    print(flag)
+
+if __name__ == '__main__':
+    main()
+
+"""
+b"Schrodinger's cat in a quantum bind, INTIGRITI{d34d_0r_4l1v3} hidden, hard to find. Is it alive, or has fate been spun? In superposition, the game's never done."
+"""
